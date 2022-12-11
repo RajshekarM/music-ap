477a6fb384c4d5e30359dde36b5914657d73a62d
rajshekar <rajshekar.mudhigonda2468@gmail.com>
2022-12-11 20:45:15 +0530

    Added-Store-Layout

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,1 +1,49 @@
- 
+ import {configureStore, createSlice} from '@reduxjs/toolkit';
+
+
+ //slice will create  action types and reducers that correspond to state.
+ const favoritesSlice = createSlice({
+                        name:'favorites',
+                        initialState:[],
+                        reducers:{
+                            addFavoriteSong(state,action)
+                            {
+                               //here state is not global state it is state that is managed by that reducer
+                                state.push(action.payload);
+                                return state;
+                            }                           
+                        }
+                    });
+
+
+
+
+const playListSlice = createSlice({
+                        name:'playLists',
+                        initialState:[],
+                        reducers:{
+                            addSong(state,action)
+                            {
+                                state.push(action.payload);
+                                return state;
+                            }                           
+                        }
+                    });
+
+
+
+const store = configureStore({
+        reducer:{ favorites:favoritesSlice.reducer , 
+                  playLists:playListSlice.reducer,               
+                }
+});
+
+                console.log(store.getState());
+
+store.dispatch({type:"favorites/addFavoriteSong",
+                payload:"john cena"});
+
+store.dispatch(playListSlice.actions.addSong('My heart will go on'))
+
+                console.log(store.getState());
+
